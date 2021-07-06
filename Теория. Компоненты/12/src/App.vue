ec54218dc46dd378dffbe47c7b3811366105c3a6
MaksShadows <Lykanenko1995@gmail.com>
2021-07-06 17:42:31 +0300

    first commit

---
+++ Теория. Компоненты/12/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <app-counter></app-counter>
+
+    <app-car></app-car>
+  </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+import Counter from './Counter.vue'
+
+export default {
+  components: {
+    appCar: Car,
+    appCounter: Counter
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Теория. Компоненты/12/src/Car.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Car</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, voluptate.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+  }
+</style>
